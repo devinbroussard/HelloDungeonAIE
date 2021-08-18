ed5ad9f2475592525d62c8aab317d8a9ac3267e7
Devin Broussard <devin.ray.broussard@gmail.com>
2021-08-18 13:00:19 -0500

    created a portion of the first area

--- HelloDungeonAIE/Game.cs
+++ HelloDungeonAIE/Game.cs
@@ -18,8 +18,8 @@ namespace HelloDungeonAIE
             string name = "";
 
             //Gets player's name
-            Console.WriteLine("Hello, what is your name?");
-            name = Console.ReadLine().ToUpper();
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
 
             //Asks player what class they'd like to choose, and gets input
             Console.WriteLine("Pick a class!");
@@ -52,6 +52,60 @@ namespace HelloDungeonAIE
             Console.WriteLine("Job: " + characterJob);
             Console.WriteLine("Health: " + health);
             Console.WriteLine("Power: " + power);
+            Console.ReadKey();
+            Console.Clear();
+
+            //First stage
+            Console.WriteLine("You awaken in a forest.");
+            Console.ReadKey();
+            Console.WriteLine("Upon looking around, you notice that the sun is setting.");
+
+            //displays different text to the player, depending on their class
+            if (characterJob == "Wizard")
+            {
+                Console.WriteLine("You look down, and see you still have your magic gear.");
+            }
+            else if (characterJob == "Knight")
+            {
+                Console.WriteLine("You look down, and see that you still have your sword and armor on.");
+            }
+
+            //Asks for player input
+            Console.WriteLine("You begin walking towards a dirt path that you see in the distance.");
+            Console.WriteLine("You reach the path, and are now faced with a decision.");
+            Console.WriteLine("Will you walk down the path to your left, or right?\n");
+            Console.WriteLine("1. Right");
+            Console.WriteLine("2. Left");
+            input = Console.ReadLine().ToLower();
+
+            //Gives the player two different outcomes, depending on the input given
+            if (input == "1" || input == "right")
+            {
+                Console.WriteLine("You walk down the path to your right, until you reach a point where the path is suddently cut off by a rushing river.");
+                Console.WriteLine("You see a fallen tree laid across the river. You might be able to cross it if you tried.");
+                Console.WriteLine("Will you turn back, or cross the tree?\n");
+                Console.WriteLine("1. Cross the Tree");
+                Console.WriteLine("2. Turn around");
+                input = Console.ReadLine().ToLower();
+
+                if (input == "1" || input == "cross" || input == "cross the tree") ;
+                {
+                    Console.WriteLine("You attempt to cross the tree.");
+                    Console.WriteLine("You only make it half way until it snaps, leaving you to drown in the rushing river.");
+                    Console.WriteLine("Game over!");
+                }
+                else if (input == "2" || input == "cross" || input == "cross the tree")
+                {
+
+                }
+
+            }
+
+            else if (input == "2" || input == "left")
+            {
+                Console.WriteLine("You down the path to your left, until you see a strange man standing in the road.");
+                Console.WriteLine("He waves at you, and then gestures for you to come closer");
+            }
 
         }
     }
